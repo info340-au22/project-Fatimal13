713e34b6aa1e66db3ae3d21b659dc613267bab61
07Eunji <eunjia@uw.edu>
2022-11-17 12:15:37 -0800

    setting up react

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function App(props) {
+  return (
+    <div>
+      <HomePage />
+      {/* <HotelPage /> */}
+      {/* <ActivityPage /> */}
+      {/* <DinningPage /> */}
+      {/* <FormPage /> */}
+      {/* <AboutPage /> */}
+    </div>
+  )
+}
+
+export default App;

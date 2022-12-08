436518f7139cb1b253fba7165c50bc682b8442a9
u8450 <rguo3.edu>
2022-12-07 17:08:18 -0800

    added questions for forms and basic styling

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { useMemo, useState } from "react";  
-// npm install react-select
-import Select from 'react-select';
+import { useState } from "react";
+import { getDatabase, ref, push as firebasePush} from 'firebase/database';
 
-    // Feedback: build everything to one component
-    // all variables should have lower cap 
-    // upload images (wait till last of class)
-
+// build user log in 
 function FormComponents(props) {
 
     // Peronsal Info
@@ -43,6 +39,15 @@ function FormComponents(props) {
     const [typedBizTypeInput, setTtypedBizTypeInput] = useState("");
     const [typedBizLocInput, setTypedBizLocInput] = useState("");
 
+    // new questions on tags, expenses, and upload images
+    const [chosenTagInput, setChosenTagInput] = useState(""); 
+    // select from the list, as check boxes or select element (drop down list)
+    //  handling changes - array of values 
+    // build up html first, then react
+
+    const [chosenExpenseInput, setChosenExpenseInput] = useState("");
+    const [imageInput, setImageInput] = useState("Select"); // from lecture
+
     const handleBizNameChange = (event) => {
         const value = event.target.value; //what user typed in
         setTypedBizNameInput(value);
@@ -58,78 +63,153 @@ function FormComponents(props) {
         setTtypedBizTypeInput(value);
     }
 
+    const handleChosenExpense  = (event) => {
+        const value = event.target.value;
+        setChosenExpenseInput(value);
+    }
+
     const handleBizLocClick = (event) => {
         const value = event.target.value;
         setTypedBizLocInput(value);
     }
 
+    const handleUploadedImg = (event) => {
+        const value = event.target.value;
+        setImageInput(value);
+    }
+
+    const handleSelectedTags = (event) => {
+        const values = event.target.values;
+        setChosenTagInput(values);
+    }
+
+    const handleSubmit = (event) =>{
+        setTypedFNameInput("");
+        setTypedLNameInput("");
+        setTypedEmailInput("");
+        setTypedPhoneInput("");
+        setTypedBizNameInput("")
+        setTypedBizDescInput("");
+        setTtypedBizTypeInput("");
+        setTypedBizLocInput("");
+        setChosenTagInput(""); 
+        setChosenExpenseInput("");
+        setImageInput("");
+        // call all set to reset everything questions
+        // create new obj = all the state variables
+        // take all state variables
+        // object.typedBizNameInput = typedBizNameInput
+        // Const myObj = { bizName : bizName, bizThing : bizThng }
+        // firebasePush 
+        // check lecture code for adding new messages
+    }
+
     return (
         <div>
             <div className='formquestion mb-3'>
-                <label>First name:  
+                <label>First name: </label>
                     <input 
                     type='text'
                     value={typedFNameInput}
                     onChange={handleFNameChange}
                     required
                     />
-                </label>
             </div>
             <div className='formquestion'>
-                <label>Last name:  
+                <label>Last name:</label>
                     <input 
                     type='text'
                     value={typedLNameInput}
                     onChange={handleLNameChange}
                     required
                     />
-                </label>
             </div>
             <div className='formquestion'>
-                <label>Email:   
+                <label>Email: </label>
                     <input 
                     type='email'
                     value={typedEmailInput}
                     onChange={handleEmailChange}
                     required
                     />
-                </label>
             </div>
             <div className='formquestion'>
-                <label>Phone number:  
+                <label>Phone number: </label>
                         <input 
                         type="tel" 
                         value={typedPhoneInput}
                         onChange={handlePhoneChange}
                         required
                         />
-                </label>
             </div>
             <div className='formquestion'>
-                <label>Business name:  
+                <label>Business name: </label> 
                     <input 
                     type="text"
                     value={typedBizNameInput}
                     onChange={handleBizNameChange}
                     required
                     />
-                </label>
             </div>
+
             <div className='formquestion col-auto'>
-                <label> Business Type:   </label>
+                <label> Business Type: </label>
                     <select className="form-select" value={typedBizTypeInput} onChange={handleBizTypeClick} >
                         <option value="hotel">Hotel</option>
                         <option value="dining">Dining</option>
                         <option value="activity">Activity</option>
-                </select>
+                    </select>
             </div>
+
+            {/* CONDITIONAL RENDERING: outside of return statement, if statement infront of return statment,
+            array of options, individual conditional rendering on options */}
+            <div className='formquestion col-auto'>
+                <label> Price: </label>
+                    <select className="form-select" value={chosenExpenseInput} onChange={handleChosenExpense} >
+                        <option value="$">$</option>
+                        <option value="$$">$$</option>
+                        <option value="$$$">$$$</option>
+                        <option value="$$$$">$$$$</option>
+                    </select>
+            </div>
+
+            <div className='formquestion col-auto'>
+                <label> Tags: </label>
+                    <select multiple className="form-select" value={chosenTagInput} onChange={handleSelectedTags} >
+                        <option value="Free Wi-Fi">Free Wi-Fi</option>
+                        <option value="Air conditioning">Air conditioning</option>
+                        <option value="Beach access">Beach access</option>
+                        <option value="Free parking">Free parking</option>
+                        <option value="Outdoor pool">Outdoor pool</option>
+                        <option value="Free breakfast">Free breakfast</option>
+                        <option value="Airport shuttle">Airport shuttle</option>
+                        <option value="Kid-friendly">Kid-friendly</option>
+                        <option value="Fitness center">Fitness center</option>
+                        <option value="Beach front">Beach front</option>
+                        <option value="Kitchen in rooms">Kitchen in rooms</option>
+                        <option value="Bar">Bar</option>
+                        <option value="Oceanfront">Oceanfront</option>
+                        <option value="Luau">Luau</option>
+                        <option value="Live Music">Live Music</option>
+                        <option value="Culture">Culture</option>
+
+                        <option value="Bar">Bar</option>
+
+
+                    </select>
+            </div>
+
+
+
+
             <div className='formquestion col-auto'>
                 <label> Business Location:   </label>
                     <select id='businessLoc' className="form-select" value={typedBizLocInput} onChange={handleBizLocClick} >
                         <option value="Maui">Maui</option>
                         <option value="Hawaiʻi">Hawaiʻi</option>
                         <option value="Oahu">Oahu</option>
-                        <option value="Lāna‘i & Molokai">Lāna‘i & Molokai</option>
+                        <option value="Molokai">Molokai</option>
+                        <option value="Lāna‘i">Lāna‘i</option>
                         <option value="Kaua'i">Kaua'i</option>
                     </select>
             </div>
@@ -144,8 +224,8 @@ function FormComponents(props) {
                 </label>
             </div>
             <button className="formquestion btn btn-primary" 
-                    type="button">Submit form
-                    {/* onClick={handleSubmit} */}
+                    type="button" onClick={handleSubmit}>Submit form
+    
             </button>
         </div>
 

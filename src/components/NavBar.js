0f0b1c1d84b195bea600124611a6167b97f0434d
07Eunji <eunjia@uw.edu>
2022-12-08 12:02:33 -0800

    sign in page added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export function NavBar(props) {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
+  const handleClick = (event) => {
+    setIsNavExpanded(!isNavExpanded);
+  }
+
   return (
-    <nav>
+    <nav className="navigation">
     <div className="logo">
       <img src="img/logo.jpg" alt="Logo" />
     </div>
-    <div className="hamburger">
+    <div className="hamburger" onClick={handleClick}>
       <div className="line1"></div>
       <div className="line2"></div>
       <div className="line3"></div>
     </div>
-    <ul className="nav-links">
-      <li><Link to="/">HOME</Link></li>
-      {/* <li><a href="index.html">HOME</a></li> */}
-      <li><Link to="/dinings">DINING</Link></li>
-      <li><Link to="/hotels">HOTELS</Link></li>
-      <li><Link to="/activities">ACTIVITIES</Link></li>
-      <li><Link to="/about">ABOUT</Link></li>
-
-      <li>
-        <Link to="">
-          <button className="login-button">Login</button>
-        </Link>
-      </li>
-      <li>
-        <Link to="/form">
-          <button className="login-button">Join</button>
-        </Link>
-      </li>
-    </ul>
+    {/* className="aLink" */}
+      <ul className={isNavExpanded ? "nav-links open" : "nav-links"}>
+        <li><Link to="/"><span className="aLink" onClick={handleClick}>HOME</span></Link></li>
+        <li><Link to="/dinings"><span className="aLink" onClick={handleClick}>DINING</span></Link></li>
+        <li><Link to="/hotels"><span className="aLink" onClick={handleClick}>HOTELS</span></Link></li>
+        <li><Link to="/activities"><span className="aLink" onClick={handleClick}>ACTIVITIES</span></Link></li>
+        <li><Link to="/about"><span className="aLink" onClick={handleClick}>ABOUT</span></Link></li>
+        <li>
+          <Link to="">
+            <button className="login-button">Login</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/form">
+            <button className="join-button">Join</button>
+          </Link>
+        </li>
+      </ul>
   </nav>
   )
 }

2625e7fa059f7473d8b42e77acc3e281b4e851d8
u8450 <rguo3.edu>
2022-12-12 15:43:28 -0800

    change in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,8 @@ export function Footer(props) {
         <div className="ft-row">
           <ul>
             <li><Link to="/about"><span>Contact us</span></Link></li>
-            <li><Link to="#"><span>Our Services</span></Link></li>
-            <li><Link to="#"><span>Privacy Policy</span></Link></li>
-            <li><Link to="#"><span>Terms & Conditions</span></Link></li>
+            <li><span>|</span></li>
+            <li><span>Data Sourced from Expedia</span></li>
           </ul>
         </div>
         <div className="ft-row">
